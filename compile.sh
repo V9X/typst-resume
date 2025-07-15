e6fdaa8344d596ca6ae1ac9435dc5f6d63a7be0f
v9x <kuba08.kp@gmail.com>
2025-07-15 21:30:23 +0200

    Include Font Awesome fonts

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# = 1 ]; then
-    typst watch --input loc=./templates/"$1" resume.typ ./output/"${1%.*}.pdf"
+    typst watch --ignore-system-fonts --font-path ./assets --input loc=./templates/"$1" resume.typ ./output/"${1%.*}.pdf"
     exit;
 fi
 
 for path in ./templates/*.yaml; do
     filename=$(basename "$path")
-    typst compile --input loc="$path" resume.typ ./output/"${filename%.*}".pdf
+    typst compile --ignore-system-fonts --font-path ./assets --input loc="$path" resume.typ ./output/"${filename%.*}".pdf
 done

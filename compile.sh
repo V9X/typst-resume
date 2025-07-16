6968445aa96215b67ba25e6086d68824fac501f5
v9x <kuba08.kp@gmail.com>
2025-07-16 20:48:16 +0200

    simplify things

--- compile.sh
+++ compile.sh
@@ -3,23 +3,13 @@
 SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &> /dev/null && pwd)"
 
 INPUT_PATH=$(realpath "${1:-"./example.yaml"}")
-INPUT_DIR=$(dirname "$INPUT_PATH")
-INPUT_FILE=$(basename "$INPUT_PATH")
-
-OUTPUT_PATH=${2:-"$INPUT_DIR/${INPUT_FILE%.*}.pdf"}
-
-ROOT_PATH=$INPUT_DIR
-FILE_PATH="/$INPUT_FILE"
-
-if [[ $INPUT_PATH == $SCRIPT_DIR* ]]; then
-    ROOT_PATH=$SCRIPT_DIR
-    FILE_PATH="${INPUT_PATH:${#SCRIPT_DIR}}"
-fi
+OUTPUT_PATH=$(realpath "${2:-${INPUT_PATH%.*}.pdf}")
 
 typst compile \
-    --root "$ROOT_PATH" \
+    --root / \
+    --creation-timestamp 1112470620 \
     --ignore-system-fonts \
     --font-path "./assets" \
-    --input "loc=$FILE_PATH" \
+    --input "loc=$INPUT_PATH" \
     "$SCRIPT_DIR/src/resume.typ" \
     "$OUTPUT_PATH"
